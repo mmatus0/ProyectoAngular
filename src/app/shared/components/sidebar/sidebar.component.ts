86eca5b780741b1ad6dda4ddff4e289d26c9e674
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2026-05-14 17:39:39 -0400

    Modificaciones en importaciones

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { AuthService } from '../../core/services/auth.service';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
